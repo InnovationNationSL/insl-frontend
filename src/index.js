6e6d18b3a72f0f65a0198d7c3b04e3177f73676d
navintc <navin.thamindu@gmail.com>
2022-06-23 00:41:00 +0530

    Adding font intergration to view1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./fonts/SF-Pro-Display-Bold.otf";
+import "./fonts/SF-Pro-Display-Regular.otf";
+import "./fonts/SF-Pro-Display-Semibold.otf";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/home/components/view1/view1.js
+++ src/pages/home/components/view1/view1.js
@@ -6,12 +6,12 @@ import ButtonCircle from "../../../../components/buttonCircle/buttonCircle";
 const View1 = () =>{
     return(
         <div className={"viewtype-1 d-flex"}>
-            <div className={"col-md-6 testbg"}>
-                <div>
-                    <p>Welcome to IEEE Innovation Nation Sri Lanka</p>
-                    <p>The excellence of guidance, mentorship and finances to take your idea or
+            <div className={"col-md-6 my-auto"}>
+                <div className={"col-md-6 mx-auto my-auto"}>
+                    <p className={"view1-title"}>Welcome to IEEE Innovation Nation Sri Lanka</p>
+                    <p className={"text-normal"}>The excellence of guidance, mentorship and finances to take your idea or
                         product to the next level.</p>
-                    <ButtonCircle text={"Join as a partner"} size={"col-md-4"}/>
+                    <ButtonCircle text={"Join as a partner"} size={"col-md-8"}/>
                 </div>
             </div>
 
