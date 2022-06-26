17c4caf1eae3a79baced531451071a0327681ab4
navintc <navin.thamindu@gmail.com>
2022-06-26 08:46:58 +0530

    Fixing View 2

--- src/pages/home/components/view2/view2.js
+++ src/pages/home/components/view2/view2.js
@@ -15,8 +15,13 @@ const View2 = () =>{
                 </div>
             </div>
 
-            <div className={"col-md-6"}>
-                <img src={atlasIcon} className={"atlasIcon"} alt={"Official icon of the year"}/>
+            <div className={"col-md-6 my-auto"}>
+                <div className={"col-md-6 mx-auto my-auto"}>
+                    <p className={"view1-title"}>Welcome to IEEE Innovation Nation Sri Lanka</p>
+                    <p className={"text-normal"}>The excellence of guidance, mentorship and finances to take your idea or
+                        product to the next level.</p>
+                    <ButtonCircle text={"Join as a partner"} size={"col-md-8"}/>
+                </div>
             </div>
         </div>
     )
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,6 +4,7 @@ import Navbar from "../../components/Navbar/Navbar";
 
 import "./home.css";
 import View1 from "./components/view1/view1";
+import View2 from "./components/view2/view2";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,10 +13,13 @@ const Home = () => {
     setIsOpen(!isOpen);
   }
   return (
-    <div>
+    <div className={"home-bg"}>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <View1 />
+        <div className={"dark-overlay"}>
+            <View2 />
+        </div>
     </div>
   );
 };
