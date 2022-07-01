c744a4a07072375e4cbf5692ffad0e2f1609df22
navintc <navin.thamindu@gmail.com>
2022-07-01 21:24:42 +0530

    Adding resource people photos and new rows

--- src/pages/home/components/resourcePeople/resourcePeople.js
+++ src/pages/home/components/resourcePeople/resourcePeople.js
@@ -8,6 +8,30 @@ import Mafaz from "./img/Mohammed Mafaz.jpg";
 import Aloka from "./img/Aloka Gunasekara.jpg";
 import Jehan from "./img/Jehan.jpg";
 
+import Chanaka from "./img/Chanaka-Hettige.jpg";
+import Chamika from "./img/Chamika.jpg";
+import Dilini from "./img/Dilini.jpg";
+import Mithushan from "./img/Mithushan Jalangan.jpg";
+import Vidura from "./img/Vidura.jpg";
+
+import Hirishegan  from "./img/Hirishegan Karuneswaran.jpg";
+import Maheshika  from "./img/Maheshika.jpg";
+import Thivvyan from "./img/thivvyan.jpg";
+import Subodha from "./img/Subodha.jpg";
+import Indika from "./img/Indika.jpeg";
+
+import Enosh from "./img/Mr. Enosh Profile.jpg";
+import Alagan from "./img/Mr. Alagan-photo-original.jpg";
+import Ashan from "./img/Mr. Ashan.jpg";
+import Suman from "./img/Mr. Suman.jpg";
+import Anushiya from "./img/Ms. Anushiya.jpg";
+
+import Tamasha from "./img/tamasha.jpeg";
+import Rangika from "./img/Prof. Rangika Halwatura 1.jpg";
+import Yasura from "./img/Mr. Yasura Samarakoon.jpg";
+import Ranjith from "./img/Eng. Ranjith G Rubasinghe.jpg";
+import Dulith from "./img/Mr. Dulith Herath.jpg";
+
 
 
 const ResourcePeople = () =>{
@@ -34,16 +58,33 @@ const ResourcePeople = () =>{
                             </div>
                         </div>
 
-
-                        <div className={"d-flex view2-info-dat"}>
+                        <div className={"d-flex view2-info-dat mx-auto"}>
                             <div className={"col-md-11 d-flex flex-sm-row flex-column"}>
-                                <Person img = {Dhammika} title="Mr. Dhammika Marasinghe" body="Activities Chair - IEEE Sri Lanka Section"/>
-                                <Person img = {Dhammika} title="Mr. Dhammika Marasinghe" body="Activities Chair - IEEE Sri Lanka Section"/>
-                                <Person img = {Dhammika} title="Mr. Dhammika Marasinghe" body="Activities Chair - IEEE Sri Lanka Section"/>
-                                <Person img = {Dhammika} title="Mr. Dhammika Marasinghe" body="Activities Chair - IEEE Sri Lanka Section"/>
+                                <Person img = {Chanaka} title="Mr. Chanaka Hettige" body="Assistant Instructor and Ph.D. Student at Indiana University Bloomington "/>
+                                <Person img = {Chamika} title="Mr. Chamika Sudusinghe" body="Secretary - IEEE Region 10 Students Activities Committee"/>
+                                <Person img = {Dilini} title="Ms. Dilini Ekanayaka" body="Co-Chairman - IEEE SL Inspire, IEEE IN Leadership Team Member"/>
+                                <Person img = {Mithushan} title="Mr. Mithushan Jalangan" body="Webmaster - IEEE Region 10 YP, IEEE IN Leadership Team Member"/>
+                                <Person img = {Vidura} title="Mr. Vidura Dhananjaya" body="IEEE SAC Membership Development Sub-Committee Chair"/>
 
                             </div>
                         </div>
+
+
+                        <div className={"d-flex view2-info-dat mx-auto"}>
+                            <div className={"col-md-11 d-flex flex-sm-row flex-column"}>
+                                <Person img = {Hirishegan} title="Mr. Hirishegan Karuneswaran" body="IT Business Analyst - PickMe"/>
+                                <Person img = {Maheshika} title="Ms. Maheshika Madubashani" body="IT Business Analyst - PayMedia"/>
+                                <Person img = {Thivvyan} title="Mr. Thivvyan Karuneswaran" body="Former Publicity Lead INSL 2020"/>
+                                <Person img = {Subodha} title="Mr. Subodha Iranga" body="Former Logistics Lead INSL 2020"/>
+                                <Person img = {Indika} title="Mr. Indika De Zoysa" body="Vice President, Enterprise Business Group,Huawei Technologies Lanka Co (Pvt) Ltd"/>
+
+                            </div>
+                        </div>
+
+                   
+
+
+ 
 
           
                     </div>
