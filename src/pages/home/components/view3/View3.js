46f43c050c9d71d47a5e1340c4203047264a7f13
NalindaGamaarachchi <44144772+NalindaGamaarachchi@users.noreply.github.com>
2022-07-05 10:26:00 +0530

    create contactUs

--- src/pages/home/components/view3/View3.js
+++ src/pages/home/components/view3/View3.js
@@ -21,36 +21,37 @@ const View3 = () => {
           <View3Icon src={Icon1} />
           <View3H2>A Platform for Growth</View3H2>
           <View3P>
-            it is a nattorm where creativitv meets lis marketolace, were vouna
-            minas dento know their opponunies and unite to learn, innovate, and
-            grow asahis a nattorm where creativitv meets lis marketolace
+            It is a platform where creativity meets its marketplace, where young
+            minds get to know their opportunities and unite to learn, innovate,
+            and grow as one.
           </View3P>
         </View3Card>
         <View3Card>
           <View3Icon src={Icon2} />
           <View3H2>Get Connected</View3H2>
           <View3P>
-            it is a nattorm where creativitv meets lis marketolace, were vouna
-            minas dento know their opponunies and unite to learn, innovate, and
-            grow asahis a nattorm where creativitv meets lis marketolace
+            IEEE Innovation Nation will Integrate students, industry
+            professionals, and academia into a collaborative space that
+            celebrates the opportunities created by tech entrepreneurship.
           </View3P>
         </View3Card>
         <View3Card>
           <View3Icon src={Icon3} />
           <View3H2>Entepreneurial Skills</View3H2>
           <View3P>
-            it is a nattorm where creativitv meets lis marketolace, were vouna
-            minas dento know their opponunies and unite to learn, innovate, and
-            grow asahis a nattorm where creativitv meets lis marketolace
+            It will provide comprehensive training focused on design thinking,
+            market analysis, financial projections, business models, soft skill
+            development, company law, technical aspects relevant to the selected
+            project ideas, etc.
           </View3P>
         </View3Card>
         <View3Card>
           <View3Icon src={Icon4} />
           <View3H2>Way to the Industry</View3H2>
           <View3P>
-            it is a nattorm where creativitv meets lis marketolace, were vouna
-            minas dento know their opponunies and unite to learn, innovate, and
-            grow asahis a nattorm where creativitv meets lis marketolace
+            The judges of the finale may double as investors, and guide the
+            teams to become noteworthy start-ups and successful entrepreneurs of
+            the future.
           </View3P>
         </View3Card>
       </View3Wrapper>
