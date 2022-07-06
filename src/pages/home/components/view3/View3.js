ba6dbc2b9182797cc7637408d6c8a25eefe63b13
NalindaGamaarachchi <44144772+NalindaGamaarachchi@users.noreply.github.com>
2022-07-06 11:41:45 +0530

    create view 3in bootstrap

--- src/pages/home/components/view3/View3.js
+++ src/pages/home/components/view3/View3.js
@@ -1,62 +1,96 @@
 import React from "react";
-import {
-  View3Container,
-  View3H1,
-  View3Wrapper,
-  View3Card,
-  View3Icon,
-  View3H2,
-  View3P,
-} from "./View3Elements";
+
+import "./View3.css";
 import Icon1 from "./Icon1.png";
 import Icon2 from "./Icon2.png";
 import Icon3 from "./Icon3.png";
 import Icon4 from "./Icon4.png";
+
 const View3 = () => {
   return (
-    <View3Container id="View3">
-      <View3H1>How will you benefit from IEEE INSL?</View3H1>
-      <View3Wrapper>
-        <View3Card>
-          <View3Icon src={Icon1} />
-          <View3H2>A Platform for Growth</View3H2>
-          <View3P>
-            It is a platform where creativity meets its marketplace, where young
-            minds get to know their opportunities and unite to learn, innovate,
-            and grow as one.
-          </View3P>
-        </View3Card>
-        <View3Card>
-          <View3Icon src={Icon2} />
-          <View3H2>Get Connected</View3H2>
-          <View3P>
-            IEEE Innovation Nation will Integrate students, industry
-            professionals, and academia into a collaborative space that
-            celebrates the opportunities created by tech entrepreneurship.
-          </View3P>
-        </View3Card>
-        <View3Card>
-          <View3Icon src={Icon3} />
-          <View3H2>Entepreneurial Skills</View3H2>
-          <View3P>
-            It will provide comprehensive training focused on design thinking,
-            market analysis, financial projections, business models, soft skill
-            development, company law, technical aspects relevant to the selected
-            project ideas, etc.
-          </View3P>
-        </View3Card>
-        <View3Card>
-          <View3Icon src={Icon4} />
-          <View3H2>Way to the Industry</View3H2>
-          <View3P>
-            The judges of the finale may double as investors, and guide the
-            teams to become noteworthy start-ups and successful entrepreneurs of
-            the future.
-          </View3P>
-        </View3Card>
-      </View3Wrapper>
-    </View3Container>
+    <section className={"view3"}>
+      <div className={"container"}>
+        <div className={"row"}>
+          <div className={"col-lg-12 text-center view3-title"}>
+            <h1 className={"title-text"}>
+              How will you benefit from IEEE INSL?
+            </h1>
+          </div>
+        </div>
+        <div className={"view3card-list"}>
+          <div className={"row"}>
+            <div className={"col-lg-3"}>
+              <div className={"view3Card text-center"}>
+                <div className={"mx-auto py-4"}>
+                  <img src={Icon1} alt={"card-1"} />
+                </div>
+                <div className={"card-body text-center"}>
+                  <h5 className={"card-title"}>A Platform for Growth</h5>
+                  <p className={"card-text text-secondary"}>
+                    It is a platform where creativity meets its marketplace,
+                    where young minds get to know their opportunities and unite
+                    to learn, innovate, and grow as one.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={"col-lg-3"}>
+              <div className={"view3Card text-center"}>
+                <div className={"mx-auto py-4"}>
+                  <img src={Icon2} alt={"card-1"} />
+                </div>
+                <div className={"card-body text-center"}>
+                  <h5 className={"card-title text-uppercase font-roboto"}>
+                    Get Connected
+                  </h5>
+                  <p className={"card-text text-secondary"}>
+                    IEEE Innovation Nation will Integrate students, industry
+                    professionals, and academia into a collaborative space that
+                    celebrates the opportunities created by tech
+                    entrepreneurship.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={"col-lg-3"}>
+              <div className={"view3Card text-center"}>
+                <div className={"mx-auto py-4"}>
+                  <img src={Icon3} alt={"card-1"} />
+                </div>
+                <div className={"card-body text-center"}>
+                  <h5 className={"card-title text-uppercase font-roboto"}>
+                    Entepreneurial Skills
+                  </h5>
+                  <p className={"card-text text-secondary"}>
+                    It will provide comprehensive training focused on design
+                    thinking, market analysis, financial projections, business
+                    models, soft skill development, company law, technical
+                    aspects relevant to the selected project ideas, etc.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={"col-lg-3"}>
+              <div className={"view3Card text-center"}>
+                <div className={"mx-auto py-4"}>
+                  <img src={Icon4} alt={"card-1"} />
+                </div>
+                <div className={"card-body text-center"}>
+                  <h5 className={"card-title text-uppercase font-roboto"}>
+                    Way to the Industry
+                  </h5>
+                  <p className={"card-text text-secondary"}>
+                    The judges of the finale may double as investors, and guide
+                    the teams to become noteworthy start-ups and successful
+                    entrepreneurs of the future.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
-
 export default View3;
